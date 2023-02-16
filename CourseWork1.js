0f92699f43c17241e217bd207628e2a863082d5e
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2023-02-16 20:27:05 +0530

    Initial commit

---
+++ CourseWork1.js
@@ -0,0 +1,98 @@
+let bucket= JSON.parse(localStorage.getItem("store")) || [];
+
+//Adding items to total quantity
+
+function increment(id) 
+{
+let selectedItem = id;
+let search = bucket.find(bucketFind)
+if (search === undefined)
+{
+   bucket.push({
+                id:selectedItem.id,
+				item:1,
+   }) 
+}
+else{
+search.item+=1;
+}
+
+//console.log(selectedItem.id);
+//console.log(bucket);
+
+localStorage.setItem("store",JSON.stringify(bucket));
+update(selectedItem.id);
+
+function bucketFind(x)
+{
+ return x.id === selectedItem.id}
+};
+
+//Removing items from total quantity
+
+function decrement(id)
+{
+let selectedItem = id;
+let search = bucket.find(bucketFind)
+
+if (search === undefined) return
+
+else if (search.item === 0) return;
+else{
+search.item-=1;
+}
+//console.log(selectedItem.id);
+//console.log(bucket);
+
+
+bucket = bucket.filter((x) => x.item !== 0);
+
+localStorage.setItem("store",JSON.stringify(bucket));
+update(selectedItem.id); 
+
+function bucketFind(x)
+{
+ return x.id === selectedItem.id}
+ 
+
+};
+
+//Update the quantity everytime add or remove items
+
+function update(id)
+{
+let search = bucket.find(bucketFind)
+//console.log(search.item)
+document.getElementById(id).innerHTML = search.item;
+calculate();
+
+function bucketFind(x)
+{
+ return x.id === id}
+
+}; 
+
+//Calculate total amount
+
+function calculate ()
+{
+let cart = document.getElementById("productsAmount");
+cart.innerHTML = bucket.map(calculateItems).reduce(calculateItems2);
+
+
+function calculateItems(y)
+{
+  return y.item
+}
+
+function calculateItems2(x,y)
+{
+  return x+y
+}
+ 
+};
+
+calculate();
+
+
+
---
+++ shopItems.js
@@ -0,0 +1,54 @@
+//Data Array of products
+
+let shopItem = [
+    {
+        id:"shirt", 
+        img:"Black_Shirt.jpeg",
+        price:"500", 
+        des:"Black Minion T-Shirt"
+    },
+    {
+        id:"shoe",
+        img:"Minion_shoe.jpeg",
+        price:"2500", 
+        des:"Black Minion Shoes"
+    },
+    {
+        id:"doll",
+        img:"Minion_doll.jpeg",
+        price:"5000", 
+        des:"Despicable Me 2 Doll"
+    },
+    { 
+        id:"hat",
+        img:"Minion_hat.jpeg",
+        price:"850", 
+        des:"Minion Beanie Knit Hat"
+    },
+    {
+        id:"doll2",
+        img:"Tall_Minion.jpeg",
+        price:"3500", 
+        des:"Despicable Me 2 Doll"
+    },
+    {
+        id:"shirt1",
+        img:"White_Shirt.jpeg",
+        price:"500", 
+        des:"White Minion T-Shirt"
+    },
+    {
+        id:"conv",
+        img:"Minions_converse.jpeg",
+        price:"1050", 
+        des:"Minions Converse"
+    },
+    {
+        id:"cap",
+        img:"Minions_Cap.jpeg",
+        price:"250", 
+        des:"Minions Baseball Hat"
+    }]
+
+
+    
